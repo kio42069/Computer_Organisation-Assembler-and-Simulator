b47fcede441e31f7511ed5161d21f7b7357aec67
Tanish Verma <astronomicaltanish@gmail.com>
2023-06-06 20:36:59 +0530

    gadar sourcing for float whilst handling 0. cases

--- float_q3.py
+++ float_q3.py
@@ -11,22 +11,43 @@ def number_to_float(val):
         flag = "Denormal"
 
     if flag=="Normal":
+        flag_1 = True
         strval = str(val)
         whole, mant = strval.split(".")
         mant=(float("0."+mant))
         dectobin_converted_val = dec_to_bin(val)
+        if dectobin_converted_val[0] == "0":
+            flag_1 = False
+        
         init_whole, init_mant = dectobin_converted_val.split(".")
 
-        exp = len(dectobin_converted_val)-1             
-        init_mant = ""
-        for i in range(len(dectobin_converted_val)):
-            if dectobin_converted_val[i] == ".":
-                exp = i-1
-                break
-            else:
-                init_mant += dectobin_converted_val[i]
+        if flag_1:
+            exp = len(dectobin_converted_val)-1             
+            init_mant = ""
+            for i in range(len(dectobin_converted_val)):
+                if dectobin_converted_val[i] == ".":
+                    exp = i-1
+                    break
+                else:
+                    init_mant += dectobin_converted_val[i]
 
-        numofmantbits = 5 - (exp)
+            numofmantbits = 5 - (exp)
+
+        else:
+            init_mant=""
+            for i in range(len(dectobin_converted_val)):
+                if dectobin_converted_val[i] == "1":
+                    exp = -(i-1)
+                    break
+
+            numofmantbits = 5
+            new_mant = dectobin_converted_val[i+1:]
+            print(new_mant, "new")
+            mant = EE.binary_to_decimal(new_mant)
+            print(mant, "conv")
+            mant = (float("0." + str(mant))) 
+            print(mant, "huh")
+
         mantarr = [0,0,0,0,0]
 
         for i in range(1, 6):
